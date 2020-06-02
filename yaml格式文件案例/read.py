6c6d4c58ac39ede2c0997291c5f4f22db2534b10
guoqin <1143908462@qq.com>
2020-06-02 11:58:43 +0800

    添加了allure报告的项目实例

--- yaml格式文件案例/read.py
+++
@@ -1,8 +0,0 @@
-import yaml
-data = open("./type_data.yaml", mode="r", encoding="utf-8")
-load_data = yaml.safe_load(data)
-print(load_data)
-for i in load_data.values():
-    print(type(i))
-
-data.close()
